ffd4f04e0faf88fc54c728759c68ce2ff9d6a34e
Abdullah Afzal <abdullah941685@gmail.com>
2026-02-06 23:44:39 +0500

    auth services updated

--- src/modules/auth/auth.validator.js
+++ src/modules/auth/auth.validator.js
@@ -4,12 +4,20 @@ const { body } = require("express-validator");
 const registerValidator = [
   body("username")
     .notEmpty()
-    .withMessage("Username is required").isLength({ min: 3 }).withMessage("Username must be at least 3 chars"),
+    .withMessage("Username is required")
+    .isLength({ min: 3 })
+    .withMessage("Username must be at least 3 chars"),
   body("email")
-    .isEmail().withMessage("Valid email is required").normalizeEmail(),
+    .isEmail()
+    .withMessage("Valid email is required")
+    .normalizeEmail(),
   body("password")
     .isLength({ min: 6 })
     .withMessage("Password must be at least 6 chars"),
+  body("referralCode")
+    .optional({ checkFalsy: true })
+    .isString()
+    .withMessage("Referral code must be a string"),
 ];
 
 const loginValidator = [
