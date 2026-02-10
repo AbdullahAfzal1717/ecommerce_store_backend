519952ef557c2ee95a1a80ac7f1cd2d5c2bb9706
Abdullah Afzal <abdullah941685@gmail.com>
2026-02-10 19:44:27 +0500

    settings model updated

--- src/models/setting.model.js
+++ src/models/setting.model.js
@@ -1,13 +1,10 @@
 const mongoose = require("mongoose");
 
-const settingsSchema = new mongoose.Schema(
-  {
-    key: { type: String, required: true, unique: true }, // e.g., "global_config"
-    cashbackPercentage: { type: Number, default: 10 }, // e.g., 10 for 10%
-    winModeEnabled: { type: Boolean, default: false }, // Control Lucky Spin
-    referralBonusEnabled: { type: Boolean, default: true },
-  },
-  { timestamps: true }
-);
+const settingsSchema = new mongoose.Schema({
+  key: { type: String, default: "global_config", unique: true },
+  cashbackPercentage: { type: Number, default: 10 },
+  winModeEnabled: { type: Boolean, default: false },
+  referralBonusEnabled: { type: Boolean, default: true },
+});
 
 module.exports = mongoose.model("Settings", settingsSchema);
